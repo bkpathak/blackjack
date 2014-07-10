70123185c60d319bd3b3fb3a8fda37071e13b16f
bkpathk <bkpathk@mtu.edu>
2014-07-10 17:32:16 -0400

    Changed fund from instance variable to static class variable

--- src/Player.py
+++ src/Player.py
@@ -3,26 +3,27 @@ class Player:
 	This class will keep track of player name, betting funds,and the bet in the current game,add fund for blackjack and win and
 	check wether player has blackjack or busted.
 	'''
+	fund = 100  # create static variable fund to play multiple round of game
+
 	def __init__(self):
-		self.fund = 100          #player initial betting fund
 		self.busted = False
 		self.bet = 0
 		self.blackjack = False
 
 	def get_fund(self):
-		return self.fund 
+		return Player.fund 
 
 	def remaining_fund(self):
-		self.fund -= self.bet
+		Player.fund -= self.bet
 
 	def is_busted(self):
 		return self.busted
 
 	def add_fund(self):
 		if self.blackjack:
-			self.fund = 1.5 * self.bet
+			Player.fund = 1.5 * self.bet
 		else:
-			self.fund += self.bet
+			Player.fund += self.bet
 
 	def current_bet(self,bet):
 		self.bet = bet
