57a7f0cef8035f798fe119352b6990f72693b7e5
bkpathk <bkpathk@mtu.edu>
2014-07-10 14:31:12 -0400

    Added is_busted method

--- src/Player.py
+++ src/Player.py
@@ -4,12 +4,16 @@ class Player:
 	'''
 	def __init__(self):
 		self.fund = 100          #player initial betting fund
+		self.busted = False
 
 	def get_fund(self):
 		return self.fund 
 
 	def remaining_fund(self,bet):
 		self.fund -= bet
+
+	def is_busted(self):
+		return self.busted
 
 	def __str__(self):
 		return "Fund " + str(self.fund)
