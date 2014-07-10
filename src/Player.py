ad2519861dd0bf8433c98dea4db35b4c31106281
bkpathk <bkpathk@mtu.edu>
2014-07-10 16:11:45 -0400

    Added the method for adding fund after win and variable to keep track of busted and blackjack

--- src/Player.py
+++ src/Player.py
@@ -1,19 +1,35 @@
 class Player:
 	'''
-	This class will keep track of player name, betting funds,and the bet in the current game.
+	This class will keep track of player name, betting funds,and the bet in the current game,add fund for blackjack and win and
+	check wether player has blackjack or busted.
 	'''
 	def __init__(self):
 		self.fund = 100          #player initial betting fund
 		self.busted = False
+		self.bet = 0
+		self.blackjack = False
 
 	def get_fund(self):
 		return self.fund 
 
-	def remaining_fund(self,bet):
-		self.fund -= bet
+	def remaining_fund(self):
+		self.fund -= self.bet
 
 	def is_busted(self):
 		return self.busted
+
+	def add_fund(self):
+		if self.blackjack:
+			self.fund = 1.5 * self.bet
+		else
+			self.fund += self.bet
+
+	def current_bet(self,bet):
+		self.bet = bet
+
+	def is_blackjack(self):
+		return self.blackjack
+
 
 	def __str__(self):
 		return "Fund " + str(self.fund)
